29258a08b3590979a0e04657e06d0e48c8a4b60d
Contengent <103345406+Contengent@users.noreply.github.com>
2022-06-03 00:30:56 -0400

    Add files via upload

---
+++ rolling-dice/src/rolling/dice/RollingDice.java
@@ -0,0 +1,20 @@
+package rolling.dice;
+
+/**
+ * @author 
+ *
+ *   /\_/\  _     ___
+ *  |>'v'<||"| -/\, ,`>
+ *     -=Edelstein=-
+ * 
+ */
+public class RollingDice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
